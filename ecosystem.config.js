91903d6e2a132b61c372328f452b9c802ee40d9e
sung-ho-park <uncompu7@gmail.com>
2021-07-07 14:08:38 +0900

    change ecosystem.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
         name: "whisper-api-gateway-server",
-        script: './src/server.js',
+        script: './src/index.js',
         watch: true,
         env: {
             "NODE_ENV": "development"
