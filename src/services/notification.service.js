6be0c04ac9149fc75e83ef8ce0703b1729e06f1e
sung-ho-park <uncompu7@gmail.com>
2021-08-25 15:20:45 +0900

    change noti router

--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -3,7 +3,6 @@ const ApiError = require('../utils/ApiError');
 const whisperAppServer = require('../utils/whisperAppServer');
 const walletAppServer = require('../utils/walletAppServer');
 const moment = require('moment')
-const {makeQuery} = require('../config/query');
 
 const getNotificationsByTimestamp = async function (data) {
     try {
@@ -14,17 +13,14 @@ const getNotificationsByTimestamp = async function (data) {
 };
 
 //todo::수정 (하나는 eqt, 하나는 weth)
-const getNotifications = async function(address, whisperTimestamp, walletTimestamp, contractAddresses){
-    const query = await makeQuery(contractAddresses);
+const getNotifications = async function(address, whisperTimestamp, walletTimestamp){
 
     whisperTimestamp ??= moment().subtract(7, 'day').unix() * 1000
     walletTimestamp ??= moment().subtract(7, 'day').unix() * 1000
     let promiseArray = [whisperTimestamp, walletTimestamp].map(async (timestamp, index) =>
         index === 0 ? Promise.resolve(await whisperAppServer.get(`/v1/notifications/${address}?timestamp=${timestamp}`).then((result)=> result.data)) :
-            Promise.resolve(await walletAppServer.get(`/v1/notifications/${address}?timestamp=${timestamp}` + query).then((result)=>result.data))
+            Promise.resolve(await walletAppServer.get(`/v1/notifications/${address}?timestamp=${timestamp}`).then((result)=>result.data))
     )
-    // const result =  await walletAppServer.get(`/v1/notifications/${address}?timestamp=${walletTimestamp}` + query)
-    // return [{data:[]},result.data];
     return await Promise.all(promiseArray)
         .then(result => result)
         .catch((err) => {throw (err)})
