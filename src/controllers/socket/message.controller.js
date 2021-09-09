74aaa0d5a781c07c622a4a2ed796ff198c22de19
deepinyellow <deepinyellow@gmail.com>
2021-09-09 12:09:44 +0900

    add socket callback in message.controller

--- src/controllers/socket/message.controller.js
+++ src/controllers/socket/message.controller.js
@@ -3,34 +3,29 @@ const {messageService, pushService} = require('../../services/index')
 const redisClient = require('../../config/database/redis')
 const config = require('../../config/config')
 
-const sendMessage = socketCatchAsync(async (io, socket, data) => {
+const sendMessage = socketCatchAsync(async (io, socket, data, callback) => {
     console.log("send message")
-    let sendMessageStatus = await messageService.sendMessage(data)
-    socket.emit('sendMessage', sendMessageStatus)
-    //todo :: 개발 후 delete
-    socket.emit("log",'sendMessage')
+    let sendMessageStatus = await messageService.sendMessage(data);
+    callback(sendMessageStatus);
+    socket.emit('sendMessage', sendMessageStatus);
 });
 
-const readMessage = socketCatchAsync(async (io, socket, data) => {
-    let readMessageStatus = await messageService.readMessage(data)
-    socket.emit('readMessage', readMessageStatus)
-    //todo :: 개발 후 delete
-    socket.emit("log",'readMessage')
+const readMessage = socketCatchAsync(async (io, socket, data, callback) => {
+    let readMessageStatus = await messageService.readMessage(data);
+    callback(readMessageStatus);
+    socket.emit('readMessage', readMessageStatus);
 });
 
-const getMessageCount = socketCatchAsync(async (io, socket, data) => {
-    let messageCounts = await messageService.getMessageCount(data)
-    socket.emit('getMessageCount', messageCounts)
-    //todo :: 개발 후 delete
-    socket.emit("log",'getMessageCount')
-    io.to('').emit('getMessageCount', messageCounts)
+const getMessageCount = socketCatchAsync(async (io, socket, data, callback) => {
+    let messageCounts = await messageService.getMessageCount(data);
+    callback(messageCounts);
+    socket.emit('getMessageCount', messageCounts);
 });
 
-const getMessagesByAddress = socketCatchAsync(async (io, socket, data) => {
-    let messages = await messageService.getMessagesByAddress(data)
-    socket.emit('getMessagesByAddress', messages)
-    //todo :: 개발 후 delete
-    socket.emit("log",'getMessagesByAddress')
+const getMessagesByAddress = socketCatchAsync(async (io, socket, data, callback) => {
+    let messages = await messageService.getMessagesByAddress(data);
+    callback(messages);
+    socket.emit('getMessagesByAddress', messages);
 });
 
 module.exports = {
