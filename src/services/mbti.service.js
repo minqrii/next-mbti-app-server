bd9e613ea803b810de2530b771e9438ad881746f
toto9602 <toto9602@naver.com>
2022-09-12 23:59:56 +0900

    feat : changePageIdx 초안

--- src/services/mbti.service.js
+++ src/services/mbti.service.js
@@ -1,12 +1,33 @@
 const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
+const { Page } = require("../models");
 
 const sendAnswer = async function (data) {
    return data;
 };
 
 const changePageIdx = async function (data) {
-   return data;
+   try {
+      const pageIdx = data.pageIdx;
+      if (pageIdx) {
+         const savedIdx = await Page.findOne({where:{pk:1}});
+
+         savedIdx.set({
+            index:pageIdx
+         })
+         await savedIdx.save();
+
+         return {
+            success : true,
+            data : {pageIdx}
+         }
+      }
+   } catch (error) {
+      return {
+         success : false,
+         data : {pageIdx}
+      }
+   }
 };
 
 module.exports = {
