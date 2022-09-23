1d5c78be8ab2312d82f5a6779e18f1068e3e2e87
양효령 <js980303@naver.com>
2022-09-24 02:21:31 +0900

    chore: final header 설정

--- src/controllers/api/mbti.controller.js
+++ src/controllers/api/mbti.controller.js
@@ -1,11 +1,13 @@
-const catchAsync = require('../../utils/catchAsync');
-const {mbtiService} = require("../../services");
+const catchAsync = require("../../utils/catchAsync");
+const { mbtiService } = require("../../services");
 
 const getMbtiResult = catchAsync(async (req, res) => {
-    const result = await mbtiService.getMbtiResult()
-    res.send(result)
-})
+  const result = await mbtiService.getMbtiResult();
+  res.setHeader("Access-Control-Allow-origin", "http://kulikelion-mbti.site");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.send({ data: result });
+});
 
 module.exports = {
-    getMbtiResult
-}
+  getMbtiResult,
+};
